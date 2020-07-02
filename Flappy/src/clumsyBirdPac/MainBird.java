f75a027cfcc8773dd842c4d8ae7f4c2859193b4e
sahilp-tech <patelsahil395@gmail.com>
2020-07-02 15:47:16 -0700

    First Commit

---
+++ Flappy/src/clumsyBirdPac/MainBird.java
@@ -0,0 +1,16 @@
+package clumsyBirdPac;
+
+import javax.swing.JFrame; 
+
+public class MainBird {
+
+	//Window for game: 1st panel has intro screen -> when clicked, goes to second panel for game
+	
+	
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+}
